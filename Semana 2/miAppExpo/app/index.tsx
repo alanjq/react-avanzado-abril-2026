c1626e842d20554a1d6f131c042c033060a00e90
Alan Jimenez <alanjq@outlook.com>
2026-04-14 22:31:39 -0500

    Ejemplo usando expo

---
+++ Semana 2/miAppExpo/app/index.tsx
@@ -0,0 +1,10 @@
+import {  View, Text, TextInput } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput keyboardType="numeric" placeholder="Ingresa el texto"></TextInput>
+    </View>
+  );
+}
